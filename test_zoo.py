ce5608c75d9b43209860585aca661d7d440c6a3d
andbar27 <andrea.barbato2796@icloud.com>
2024-06-05 13:29:35 +0200

    tests andbar27

--- test_zoo.py
+++ test_zoo.py
@@ -43,6 +43,72 @@ class TestZoo(TestCase):
         
         self.assertEqual(result, 1, message)
         
+    def test_animal_dimension_andbar27(self):
+        self.zk = ZooKeeper("gino","paoli","gp001")
+        fence_1 = Fence(0, 0, "Savana")
+        animal_1 = Animal("pino", "Cane", 1, 1, 1, "Savana")
+        zookeeper_1 = ZooKeeper("gino", "paoli", "gp001")
+        zookeeper_1.add_animal(animal_1, fence_1)
+        result = len(fence_1.animals)
+        message = f"Error: area is 0 so the animal should not add in the fence"
+        self.assertEqual(result, 0, message) # Se result = 0 scrivi message
+
+    def test_animal_habitat_andbar27(self):
+        self.zk = ZooKeeper("gino","paoli","gp001")
+        a2 = Animal("luna", "gatto", 7, 2, 2, "Casa")
+        f2 = Fence(10, 10, "Savana")
+        self.zk.add_animal(a2, f2)
+        result = len(f2.animals)
+        message = "Error: habitat is different, so the animal should not add in the fence"
+        self.assertEqual(result, 0, message)
+        
+    def test_clean_andbar27(self):
+        self.zk = ZooKeeper("gino","paoli","gp001")
+        a2 = Animal("luna", "gatto", 7, 2, 2, "Savana")
+        f2 = Fence(10, 10, "Savana")
+        self.zk.add_animal(a2, f2)
+        result = self.zk.clean(f2)
+        message = "Error: result of clean is not right, Expected 0.667"
+        self.assertEqual(result, 0.667, message)
+
+        a2 = Animal("luna", "gatto", 7, 2, 2, "Savana")
+        f2 = Fence(4, 10, "Savana")
+        self.zk.add_animal(a2, f2)
+        result = self.zk.clean(f2)
+        message = "Error: result of clean is not right, Expected: 4"
+        self.assertEqual(result, 4, message)
+
+    def test_feed_andbar27(self):
+        self.zk = ZooKeeper("gino","paoli","gp001")
+        a1 = Animal("a1", "coniglio", 1, 2, 2, "gabbia")
+        f1 = Fence(4, 1, "gabbia")
+        self.zk.add_animal(a1, f1)
+        self.zk.feed(a1)
+        result = a1.height
+        message = "Error: height must be already 2, because there isn't space in fence, Expected: 2"
+        self.assertEqual(result, 2, message)
+
+        a1 = Animal("a1", "coniglio", 1, 2, 2, "gabbia")
+        f1 = Fence(5, 1, "gabbia")
+        self.zk.add_animal(a1, f1)
+        self.zk.feed(a1)
+        result = a1.height
+        message = "Error: height must be height + 2%, Expected: 2.04"
+        self.assertEqual(result, 2*1.02, message)
+    
+    def test_remove_andbar27(self):
+        self.zk = ZooKeeper("gino","paoli","gp001")
+        a1 = Animal("a1", "coniglio", 1, 2, 2, "gabbia")
+        f1 = Fence(4, 1, "gabbia")
+        a2 = Animal("a2", "uccello", 1, 1, 1, "gabbia")
+        self.zk.add_animal(a1, f1)
+        result = False
+        try:
+            self.zk.remove_animal(a2, f1)
+        except Exception:
+            result = True
+        message = "Runtime error, try to remove animal not present"
+        self.assertEqual(result, False, message)
     
     
 
